12112dfaea7dd12795ba31a7f3eff45a8eeb8ba0
girishpatil <girishgoudap0@gmail.com>
2025-01-21 14:45:54 +0530

    it is math related code

---
+++ stat/stat1.py
@@ -0,0 +1,19 @@
+import statistics
+
+data=[1,2,3,4,5,6,7,8,9,10]
+
+# Mean
+mean=statistics.mean(data)
+print("Mean:",mean)
+
+# Median
+median=statistics.median(data)
+print("Median:",median)
+
+# Mode
+mode=statistics.mode(data)
+print("Mode:",mode)
+
+#standard deviation
+std_dev=statistics.stdev(data)
+print("Standard Deviation:",std_dev)
