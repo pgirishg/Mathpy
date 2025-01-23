4ea5a8098cc2326b4f5cfa1005c4a4059b30c264
girishpatil <girishgoudap0@gmail.com>
2025-01-23 20:19:55 +0530

    sine function

---
+++ stat/funvis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#define x vlues
+x=np.linspace(0,10,100)
+
+#define y values
+y=np.sin(x)
+
+#plot the function
+plt.plot(x,y)
+plt.title("sine function")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.grid(True)
+plt.show()
